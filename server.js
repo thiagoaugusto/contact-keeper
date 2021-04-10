6e7c964f12ce5c8a6ded0af2120206608038ec22
thiagoaugusto <thiago.android88@gmail.com>
2021-04-10 19:19:15 -0300

    Backend Routes

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const users = require('./routes/users');
+const auth = require('./routes/auth');
+const contacts = require('./routes/contacts');
 
 const app = express();
 
-app.get('/', (req, res, next) => {
-  res.status(200).json({ msg: 'Welcome to ContactKeeper API' });
-});
+// Define Routes
+app.use('/api/users', users);
+app.use('/api/auth', auth);
+app.use('/api/contacts', contacts);
 
 const PORT = process.env.PORT || 5000;
 
