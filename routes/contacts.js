eb3ea4b7ea83997f893e0c7c901f7b98e7a80a4b
thiagoaugusto <thiago.android88@gmail.com>
2021-04-12 23:19:36 -0300

    Add Contact Route

--- routes/contacts.js
+++ routes/contacts.js
@@ -11,23 +11,50 @@ const Contact = require('../models/Contact');
 // @access  Private
 router.get('/', auth, async (req, res, next) => {
   try {
-    const contacts = await Contact.find({ user: req.user.find }).sort({
+    const contacts = await Contact.find({ user: req.user.id }).sort({
       date: -1,
     });
 
     res.status(200).json({ contacts });
   } catch (err) {
     console.log(err.message);
-    res.status(500).json({ msg: err.message });
+    res.status(500).json({ msg: 'Server error' });
   }
 });
 
 // @route   POST api/contacts
 // @desc    Add new contact
 // @access  Private
-router.post('/', (req, res, next) => {
-  res.send('Add new contact');
-});
+router.post(
+  '/',
+  [check('name', 'Name is required').not().isEmpty()],
+  auth,
+  async (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const { name, email, phone, type } = req.body;
+
+      const newContact = new Contact({
+        name,
+        email,
+        phone,
+        type,
+        user: req.user.id,
+      });
+
+      const contact = await newContact.save();
+
+      res.status(201).json({ contact });
+    } catch (err) {
+      console.log(err.message);
+      res.status(500).json({ msg: 'Server error' });
+    }
+  }
+);
 
 // @route   PUT api/contacts/:id
 // @desc    Update contact
