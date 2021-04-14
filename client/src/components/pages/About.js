a67f2a84a9561b6dbe809f269d5da0c6af8c6623
thiagoaugusto <thiago.android88@gmail.com>
2021-04-13 22:13:45 -0300

    Cleanup, Navbar & Router Setup

---
+++ client/src/components/pages/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p class='my-1'>This is full stack React app for keeping contacts</p>
+      <p className='bg-dark p'>
+        <strong>Version: </strong> 1.0.0
+      </p>
+    </div>
+  );
+};
+
+export default About;
