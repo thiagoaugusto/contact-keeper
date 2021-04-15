87bf8e3a844d321d130934f948868e92dd41d221
thiagoaugusto <thiago.android88@gmail.com>
2021-04-15 14:07:40 -0300

    ContactForm Component

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Contacts from '../contacts/Contacts';
+import ContactForm from '../contacts/ContactForm';
 
 const Home = () => {
   return (
     <div className='grid-2'>
-      <div>{/* Contact Form */}</div>
       <div>
-        <Contacts></Contacts>
+        <ContactForm />
+      </div>
+      <div>
+        <Contacts />
       </div>
     </div>
   );
