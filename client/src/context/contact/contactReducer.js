3d7d117b418d080baf2428f29420e29b244d7515
thiagoaugusto <thiago.android88@gmail.com>
2021-04-15 22:10:12 -0300

    Set & Clear Current Contact

--- client/src/context/contact/contactReducer.js
+++ client/src/context/contact/contactReducer.js
@@ -22,6 +22,16 @@ export default (state, action) => {
           return contact.id !== action.payload;
         }),
       };
+    case SET_CURRENT:
+      return {
+        ...state,
+        current: action.payload,
+      };
+    case CLEAR_CURRENT:
+      return {
+        ...state,
+        current: null,
+      };
     default:
       return state;
   }
